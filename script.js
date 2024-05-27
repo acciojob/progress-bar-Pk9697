3772ba13a34a632a3331a7850f9d57f97851866a
Prashant Kumar <38308359+Pk9697@users.noreply.github.com>
2024-05-27 16:43:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,43 @@
 //your JS code here. If required.
+//your JS code here. If required.
+const prevBtn=document.getElementById('prev')
+const nextBtn=document.getElementById('next')
+
+let currentCircleCount=1
+
+nextBtn.addEventListener('click',()=>{
+	if(currentCircleCount+1<=5){
+		const currentCircle=document.getElementById(`circle-${currentCircleCount+1}`)
+		currentCircle.classList.add('active')
+		++currentCircleCount
+	}
+
+	if(currentCircleCount==5){
+		nextBtn.setAttribute("disabled","")
+	}else{
+		nextBtn.removeAttribute("disabled")
+	}
+
+	if(currentCircleCount>1){
+		prevBtn.removeAttribute("disabled")
+	}
+})
+
+prevBtn.addEventListener('click',()=>{
+	if(currentCircleCount>1){
+		const currentCircle=document.getElementById(`circle-${currentCircleCount}`)
+		currentCircle.classList.remove('active')
+		--currentCircleCount
+	}
+
+	if(currentCircleCount==1){
+		prevBtn.setAttribute("disabled","")
+	}else{
+		prevBtn.removeAttribute("disabled")
+	}
+
+	if(currentCircleCount<5){
+		nextBtn.removeAttribute("disabled")
+	}
+})
+
